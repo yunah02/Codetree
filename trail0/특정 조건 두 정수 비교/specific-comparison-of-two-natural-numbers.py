fd0528741f61783a23fee803a23d2bfec71b8f9c
윤아 <145113747+yunah02@users.noreply.github.com>
2026-08-04 22:38:41 +0900

    [Passed] 특정 조건 두 정수 비교 (53ms, 16MB)

---
+++ trail0/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,18 @@
+inp = input().split()
+
+a = int(inp[0])
+b = int(inp[1])
+
+if a < b:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
+
+if a == b:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
+
+
+
+
