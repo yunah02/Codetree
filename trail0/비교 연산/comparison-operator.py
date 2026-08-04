5d7a389cf54150a8524eb00d5be8e386cb2266d5
윤아 <145113747+yunah02@users.noreply.github.com>
2026-08-04 15:07:39 +0900

    [Passed] 비교 연산 (50ms, 16MB)

---
+++ trail0/비교 연산/comparison-operator.py
@@ -0,0 +1,35 @@
+inp = input()
+arr = inp.split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+if a>=b:
+    print(1)
+else:
+    print(0)
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if a<=b:
+    print(1)
+else:
+    print(0)
+
+if a<b:
+    print(1)
+else:
+    print(0)
+
+if a == b:
+    print(1)
+else:
+    print(0)
+
+if a!=b:
+    print(1)
+else:
+    print(0)
